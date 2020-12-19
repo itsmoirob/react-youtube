774e87d284777b11f42588603290c404d6c5dc15
Robbie <rob.w.sherman@gmail.com>
2020-12-19 07:25:04 +0000

    install axios add youtube api

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'redaxios';
+
+const KEY = 'AIzaSyAcu_qOGUhxSV6PPDGICMjQncgbAO6cac8';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
